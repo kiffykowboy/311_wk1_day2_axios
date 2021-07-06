dc20b4f25a85cc5b7eeb178fe488a53769d90165
kiffykowboy <chris@Chriss-MacBook-Pro.local>
2021-07-06 15:12:44 -0500

    tests passing

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 // import axios here
+const axios = require('axios').default;
 
 // Please note that it is normally not considered best practice to commit 
 // api keys to github as it presents a security risk. It is done here only 
@@ -8,17 +9,37 @@ const api_key = 'd771b19ef336ed8381def3a60b574464'
 const discoverMovie = () => {
   const url = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}`
   // code here
+  return axios.get(url);
 }
 
 const getMovieById = (id) => {
   const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${api_key}`
   // code here
+return axios(url)
+.then( function(results) {
+  console.log(results.data);
+  return results.data
+});
+
+
+  
 }
 
 const getMovieByIdFailure = () => {
-  const fakeId = 1 // FAKE ID HERE
+  const fakeId = 5783 // FAKE ID HERE
   const url = `https://api.themoviedb.org/3/movie/${fakeId}?api_key=${api_key}`
   // code here
+  return axios(url)
+  .then( function(results) {
+    console.log('res', results.data);
+    return results.data
+  })
+  .catch(function (error) {
+    // handle error
+    console.log('error', error);
+    return error.response.status
+  })
+;
 }
 
 
